3a6c9922456217b20df9729023a696dd2936c4e8
Sumiko Nakazawa <snakazawa@otobank.co.jp>
2015-05-13 17:53:00 +0900

    wikiか掲示板らしき物をつくり途中の練習ページを移行させた

---
+++ bbs/index.php
@@ -0,0 +1,32 @@
+<?php
+if (isset($_POST['wikicontents'])) {
+  $wikicontents = nl2br($_POST['wikicontents']);
+} else {
+  $wikicontents = "";
+}
+file_put_contents("wiki_storage.txt" , $wikicontents);
+$storage = file_get_contents("wiki_storage.txt");
+?>
+
+<html>
+<head>
+  <link rel="stylesheet" type="text/css" href="wiki.css" media="all">
+  <title>PHP_Practice -wiki-</title>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+</head>
+<body>
+  <ul id="gNavi">
+    <li><a href="http://localhost/wiki/index.php">記載内容</a></li>
+    <li><a href="http://localhost/wiki/wikiedit.php">編集画面</a></li>
+  </ul>
+  <p>--記述内容--</p>
+  <p><?php echo($storage); ?></p>
+  <br>
+  <p>--編集--</p>
+  <form method="post">
+  <textarea name="wikicontents" rows="10" cols="80">
+<?PHP echo($wikicontents); ?></textarea><br>
+  <input type="submit" name="done" value="編集を終了">
+  </form>
+</body>
+</html>
