a4d6028cc3f67f9579724d8a7879bbac7d07445e
sumiko <syumikko@gmail.com>
2015-05-13 21:45:16 +0900

    練習用 HTMLのみ

---
+++ omikuji/index2.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="utf-8">
+    <title>おみくじ</title>
+</head>
+<body>
+    <h1>おみくじ</h1>
+    <p>今日の運勢 「」！</p>
+    <p><a href="">もう一度</a></p>
+</body>
+</html>
+
